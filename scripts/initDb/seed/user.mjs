69d58d08d17a5ea026355d24ac09bafac51c3a4e
jumpeace <kawahara@katfuji.lab.tuat.ac.jp>
2025-09-24 19:04:26 +0900

    refactor: .secret_data_branches → .secrets

--- scripts/initDb/seed/user.mjs
+++ scripts/initDb/seed/user.mjs
@@ -1,6 +1,6 @@
 import fs from 'fs';
 
 export default function getSeedUsers(envName) {
-  const seedUsersContents = fs.readFileSync(`.secret_data_branches/seedUsers/${envName}.json`, 'utf8');
+  const seedUsersContents = fs.readFileSync(`.secrets/seedUsers/${envName}.json`, 'utf8');
   return JSON.parse(seedUsersContents);
 }
